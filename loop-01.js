1260721731c6b45e23236166c2ef4b36dc5ec47e
Ayaal Adamov <aiaal.adamov@gmail.com>
2019-11-12 15:19:03 +0900

    Убрал счетчик

--- loop-01.js
+++ loop-01.js
@@ -3,5 +3,4 @@
 var N = 100;
 for (let i = 0; i<N; i++) {
 	console.log('Hello, world!');
-	console.log(i+1);
 }
--- loop-02.js
+++ loop-02.js
@@ -2,6 +2,4 @@
 let i = 0;
 while (i<100){
 	console.log('Hello, world!');
-	console.log(i+1);
-	i++;
 }
