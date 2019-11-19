b7bf5825d11773b47a0c5f446506f3b674db9ce2
keltoblast <55118185+keltoblast@users.noreply.github.com>
2019-11-19 11:23:05 +0900

    лаб.6 тест 2 сделал только что

--- loop-02.js
+++ loop-02.js
@@ -1,1 +1,7 @@
-// Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла do ... while.
+var x=100;
+var i=0;
+do{
+console.log(i+ "hello woorld");
+i++
+}
+while (i <x);// Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла do ... while.
