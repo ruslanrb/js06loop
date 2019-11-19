ff2939def1722ac2c357597bb037229502003c4a
Денис Осипов <djspektr2001@mail.ru>
2019-11-19 10:33:43 +0900

    3

--- loop-03.js
+++ loop-03.js
@@ -1,8 +1,10 @@
 function gcd(m,n) {
-	var res;
 	// напишите код нахождения наибольшего общего делителя
-
-	return res;
+	while (m!=0 && n!=0) 
+	{ if (m>=n) m = m%n;
+		else n = n % m;}
+	if (m==0) console.log(n);
+			else console.log (m);
 }
 
 module.exports = gcd;
