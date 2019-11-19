98354c073e6643cbd536089c1da3c22fa85ce8b7
sturtle333 <sturtle333@gmail.com>
2019-11-19 10:41:41 +0900

    сделал

--- loop-01.js
+++ loop-01.js
@@ -1,4 +1,4 @@
-// Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла for
-for (var i = 0; i < 100; i++) {
-	console.log("Hello world")
+var i;
+for (i=0; i<100; i++) {
+console.log(i + "Hello world");
 }
