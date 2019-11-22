8a6ff39217ec336f634b6c6d6851e20543a89b25
Ayhal <54825050+Ayhal@users.noreply.github.com>
2019-11-23 01:28:25 +0900

    Сделал 5

--- loop-05.js
+++ loop-05.js
@@ -1,5 +1,29 @@
-function checkCardNumber(nstr) {
+function f(n) {
     // проверка номера кредитной карты
+var a,c;
+var b=0;
+var d=0;
+var h=n;
+var v=0;
+for(i=1000000000000000;i>1;i=i/100){
+a=Math.trunc(n/i);
+if (a<5){
+b=(a*2)+b;
+}else{
+b=(1+((a*2)%10)+b)
 }
+n=n-(Math.trunc(n/(i/10))*i/10)}
 
-module.exports = checkCardNumber;
+for(i=100000000000000;i>=1;i=i/100){
+c=Math.trunc(h/i);
+v=c%10;
+d=v+d;
+}
+console.log(b,d);
+if ((b+d)%10==0){
+return "True"
+}else{
+return "False"
+}
+}
+module.exports = f;
