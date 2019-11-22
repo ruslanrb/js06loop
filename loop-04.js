7e906b275328f7e5196a2420d43552650f106d90
Ayhal <54825050+Ayhal@users.noreply.github.com>
2019-11-23 00:03:56 +0900

    Сделал 4

--- loop-04.js
+++ loop-04.js
@@ -4,12 +4,18 @@ function f(n) {
 	// должны быть в начале строки, но не в конце
 var a="";
 var b="";
-for (var i=1;i<(n+1);i++){
-a="";
-b=b+(a+res+"\n");
+if (0<n && n<21){
+for(var i=n;i>0;i--){
+for(var c=i;c>0;c--){
+a=a+" ";
+}
+b=b+a+res+"\n";
 res=res+"#"+"#";
-
-console.log(b,i)}
-	}
-
+a="";
+}
+console.log(b)
+}else{
+return "Выберите числа от 1-го до 20-ти"
+}
+}
 module.exports = f;
