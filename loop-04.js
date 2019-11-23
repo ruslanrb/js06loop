bf7879ddeeee45b79bbd5d13d88822eb4b8531d8
Анатолий Самойлов <cendza@mail.ru>
2019-11-23 17:04:19 +0300

    Сделал 4

--- loop-04.js
+++ loop-04.js
@@ -1,17 +1,22 @@
 function pyramid(n) {
-	var res = ""
-
-	for (var i = 0; i < n; i++) {
-		for (var k = 1; k <= n-i-1; k++) {
-			res += " "
+	a = " ";
+	b = "#";
+	c = "\n";
+	s = "";
+	if ((n >= 1)&&(n <= 20)) {
+		for(i = 0; i < n; i++) {
+			for(j = 0; j < n - i - 1; j++) { 
+				s += a;
+			}
+			for(j = 0; j < 2 * i + 1; j++) { 
+				s += b;
+			}
+			if (i < n -1) s += c;
 		}
-		for (var x = 1; x <= (i*2)+1; x++) {
-			res += "#"
-		}
-		if (i < n-1) {res+="\n"}
+	}else{
+		console.log("Введите число от 1 до 20")
 	}
-
-	return res
+	return s;
 }
 
 module.exports = pyramid;
