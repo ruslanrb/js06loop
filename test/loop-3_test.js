4e17de88eca191571b6c555c96ddee4ecced070b
Анатолий Самойлов <cendza@mail.ru>
2019-11-23 17:12:04 +0300

    Сделал тест 3

--- test/loop-3_test.js
+++ test/loop-3_test.js
@@ -1,13 +1,15 @@
 const gcd = require('../loop-03.js');
 
-QUnit.test("gcd(m, n) найти наибольший общий делитель", 
+QUnit.test("gcd(m, n) найти наибольший общий делитель ", 
 	function(assert){
-		assert.deepEqual(gcd(66, 22), 22, "Не выполнено");
+		assert.deepEqual(gcd(44, 22), 22, "Не выполнено");
 		assert.deepEqual(gcd(16, 16), 16, "Не выполнено");
 		assert.deepEqual(gcd(0, 52), 0, "Не выполнено");
 		assert.deepEqual(gcd(8, 3), 1, "Не выполнено");
 		assert.deepEqual(gcd(-5, 3), 1, "Не выполнено");
 		assert.deepEqual(gcd(-80, -16), 16, "Не выполнено");
 		assert.deepEqual(gcd(36, -9), 9, "Не выполнено");
+		assert.deepEqual(gcd(4, -4), 4, "Не выполнено");
+
 	}
 );
