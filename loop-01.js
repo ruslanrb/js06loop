c89bbd8af201cd133f90f9f065a715fe920115d8
Анатолий Самойлов <cendza@mail.ru>
2019-11-23 16:48:01 +0300

    Сделал 1,2

--- loop-01.js
+++ loop-01.js
@@ -1,3 +1,3 @@
 for (var i = 0; i < 100; i++) {
-	console.log("Hello world")
+	console.log("Hello world!")
 }
--- loop-02.js
+++ loop-02.js
@@ -1,6 +1,6 @@
 var i=0;
 do {
-	console.log("Hello world")
+	console.log("Hello world!")
 	i++
 }
 while (i < 100)
