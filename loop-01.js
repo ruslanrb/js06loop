3b4e570cd28ac7c2fdc192dda89688152d58a288
Вешников Валерий <Valeraiq@mail.ru>
2019-11-23 17:31:42 +0900

    Исправил  надпись

--- loop-01.js
+++ loop-01.js
@@ -1,7 +1,7 @@
 // Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла for.
 
 function HW(){
-	for (let a = 0; a < 100; a++){
+	for (let a = 0;  a < 100; a++){
 		console.log("Hello world!");
 	}	
 }
