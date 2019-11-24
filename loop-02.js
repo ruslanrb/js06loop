5b8643c5a72669984be0ba6393edf2e62dca4512
rggrgr <33679187+rggrgr@users.noreply.github.com>
2019-11-24 15:57:44 +0900

    Update loop-02.js

--- loop-02.js
+++ loop-02.js
@@ -1,7 +1,6 @@
 // Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла do ... while.
 let i = 0;
-while (i<100){
-	console.log('Hello, world!');
-	console.log(i+1);
+do{
+	print("Hello World!");
 	i++;
-}
+}while (i < 100)
