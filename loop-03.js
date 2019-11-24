b3c041092d964c894c6412c0346c4ec8fdfc8cb3
ganyavasiliev <54825242+ganyavasiliev@users.noreply.github.com>
2019-11-24 16:06:08 +0900

    исправил

--- loop-03.js
+++ loop-03.js
@@ -1,13 +1,13 @@
-function gcd(m, n) {
+function gcd(m,n) {
 	var res;
-  if(m==0 || n==0){
+	if(m==0 || n==0){
 		return 0;
 	}
 	if(m==1 || n==1){
 		return 1;
 	}
-  while(n!== 0) n = m %(m = n);
-  return m;
+	while(n!== 0) n = m %(m = n);
+	return m;
 	return res;
 }
 module.exports = gcd;
