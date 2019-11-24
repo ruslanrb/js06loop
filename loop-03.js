63acb2402c06493e2b82c2d97504ffb42efb62df
Николаев <sandalnikolaev160@gmail.com>
2019-11-24 21:55:20 +0900

    сделал

--- loop-03.js
+++ loop-03.js
@@ -1,8 +1,21 @@
 function gcd(m,n) {
 	var res;
 	// напишите код нахождения наибольшего общего делителя
-
-	return res;
+do {
+			if (Math.abs(m)>Math.abs(n))
+			{
+				m=Math.abs(m)-Math.abs(n);
+			}
+			if (Math.abs(n)>Math.abs(m))
+			{
+				n=Math.abs(n)-Math.abs(m)
+			}
+			if (Math.abs(n)==Math.abs(m))
+			{
+				res=Math.abs(m);	
+			return res;
+			}
 }
-
+	while ((Math.abs(m)>Math.abs(n)) || (Math.abs(m)<Math.abs(n)))
+}	
 module.exports = gcd;
