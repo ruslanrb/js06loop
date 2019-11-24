f2eb0a4ce0404e48338db48f4b6336ac6c44765c
Ариан Михайлов <amixailov668@gmail.com>
2019-11-24 23:49:58 +0300

    Сделал 4

--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,21 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
-
-	return res;
+	var s = "";
+	var i = 0;
+	var q = 0;
+	var z = 0;
+	if ((n<1)||(n>20)) {
+		return("Error!");
+	}
+	for(i;i<n;i++) {
+		for(q=1;q<=n-i-1;q++)
+		{
+			s+=" "
+		}
+		for(z=1;z<=i*2+1;z++) {
+			s+="#"
+		}
+		if(i<n-1){s+="\n"}
+	}
+	return s
 }
-
 module.exports = pyramid;
