3b7831a47a95b9fdfe753dd9812a1b8f26565b02
Aleksandr Pavlov <26275111+saaska@users.noreply.github.com>
2019-11-24 23:29:13 +0900

    del

--- loop-02.js
+++ loop-02.js
@@ -1,7 +1,1 @@
 // Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла do ... while.
-let i = 0;
-while (i<100){
-	console.log('Hello, world!');
-	console.log(i+1);
-	i++;
-}
