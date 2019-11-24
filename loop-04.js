134401f43566f7a3e74fc698686c7a51aeeaa899
Никита <nik200211@gmail.com>
2019-11-24 21:51:28 +0900

    все сделал

--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,21 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
-
-	return res;
+	a = " ";
+	b = "#";
+	c = "\n";
+	s = "";
+	if ((n >= 1)&&(n <= 20)) {
+		for(i = 0; i < n; i++) {
+			for(j = 0; j < n - i - 1; j++) { 
+				s += a;
+			}
+			for(j = 0; j < 2 * i + 1; j++) { 
+				s += b;
+			}
+			if (i < n -1) s += c;
+		}
+	
+	}
+	return s;
 }
 
 module.exports = pyramid;
---
+++ test/loop-5_test.js
@@ -0,0 +1,12 @@
+const checkCardNumber = require('../loop-05.js');
+
+QUnit.test("Задача 05, проверка карты", 
+	function(assert){
+		assert.deepEqual(checkCardNumber("4372282244310005"), true, "Не выполнено 4372282244310005");
+		assert.deepEqual(checkCardNumber("2200550150171302"), true, "Не выполнено 2200550150171302");
+		assert.deepEqual(checkCardNumber("12030101010413123"), "Error", "Не выполнено 12030101010413123");
+		assert.deepEqual(checkCardNumber("1203010101010101"), false, "Не выполнено 1203010101010101");
+		assert.deepEqual(checkCardNumber("12030101000413124"), "Error", "Не выполнено 12030101000413124");
+		assert.deepEqual(checkCardNumber("22030101010413123"), "Error", "Не выполнено 12030101010413123");
+	}
+);
