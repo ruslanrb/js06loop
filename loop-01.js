e2cf4b090f0cac984a4f9ed9cd88d9a03bdeb65c
Vasilyeva <bdcapcap@gmail.com>
2019-11-24 16:32:25 +0900

    lab6

--- loop-01.js
+++ loop-01.js
@@ -1,7 +1,5 @@
 // Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла for.
 
-var N = 100;
-for (let i = 0; i<N; i++) {
+for (var i = 0; i<100; i++) {
 	console.log('Hello, world!');
-	console.log(i+1);
 }
--- loop-02.js
+++ loop-02.js
@@ -1,7 +1,6 @@
 // Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла do ... while.
-let i = 0;
+var i = 0;
 while (i<100){
 	console.log('Hello, world!');
-	console.log(i+1);
 	i++;
 }
