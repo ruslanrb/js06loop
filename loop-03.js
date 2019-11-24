5abd0fc0e9b80dcdfa545e564bcab91efaeea274
rggrgr <33679187+rggrgr@users.noreply.github.com>
2019-11-24 15:58:12 +0900

    Update loop-03.js

--- loop-03.js
+++ loop-03.js
@@ -1,22 +1,20 @@
 function gcd(m,n) {
 	var res;
+	var a;
 	// напишите код нахождения наибольшего общего делителя
-	if ((m===0)||(n===0)){
-		return "АААаааа! Обнаружен НОООЛЬ!";
+	if(m == n){
+		res = m;
+		return res;
+	}	
+	while(m != n){
+		if(m > n){
+			m = m - n;
+		}else{
+			n = n - m;
+		}
+		res = m;
 	}
-	else {
-		while(m!=n){
-		m=Math.abs(m)
-		n=Math.abs(n)
-		if (m>n) {
-			m=m-n
-		}
-		else if (m<n){
-			n=n-m}
-		}
-	res=m
 	return res;
-}
 }
 
 module.exports = gcd;
