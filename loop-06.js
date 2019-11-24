632f5db5e0d4394ad9b40e2123f0c4d212053f35
rggrgr <33679187+rggrgr@users.noreply.github.com>
2019-11-24 15:56:08 +0900

    Update loop-06.js

--- loop-06.js
+++ loop-06.js
@@ -1,5 +1,23 @@
 function expDiff(nstr) {
     // модуль разности между стандартной экспонентой и приближением в виде ряда
+    var e = 1; // счетчик 
+    var x = nstr; // степень е
+    var w = 1; // отношение степени и факториала
+    var fac = 1; // факториал
+    var exp = -1; // сумма
+    while(w > 0.0001){
+    	fac = 1;
+    	x = 1;
+    	// находение одного члена 
+    	for (var i = 1; i < e; i++) {
+    		x = x * nstr;
+    		fac = fac * i;
+    	}
+    	exp = exp + w;
+    	w = x / fac;
+    	e++;
+    }
+    return(Math.exp(nstr) - exp);
 }
 
 module.exports = expDiff;
