4c8052421f3808bec4a6b530747fc3823509529a
Ustinov-Ki <54973177+Ustinov-Ki@users.noreply.github.com>
2019-11-24 22:34:48 +0900

    Add files via upload

--- loop-03.js
+++ loop-03.js
@@ -1,19 +1,17 @@
-// напишите код нахождения наибольшего общего делителя
-//node loop-03.js
-//function gcd(m,n) {
-	//var res;
-	m = 30
-	n = 18
-	while (m != 0 & n != 0 ){
-		if (m > n) {
-			m = m % n
+function gcd(m,n) {
+	var res;
+	// напишите код нахождения наибольшего общего делителя
+	while(m != 0 && n != 0){
+		if (m > n){
+			m %= n;
 		}
-		else {
-			n = n % m
-			}
+		else{
+			n %= m;
+		}
 	}
-	//return res;
-	console.log("10")
-//}
 
-//module.exports = gcd;
+	res = m + n;
+	return res;
+}
+
+module.exports = gcd;
