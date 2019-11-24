b666f429ccc05e0cea3865ccd6cd62ca2a6c52bf
rggrgr <33679187+rggrgr@users.noreply.github.com>
2019-11-24 15:58:33 +0900

    Update loop-04.js

--- loop-04.js
+++ loop-04.js
@@ -1,7 +1,15 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
+	var res = "";
+
+	for (var i = 0; i < n; i++) {
+		for (var k = 1; k <= n-i-1; k++) {
+			res += " ";
+		}
+		for (var x = 1; x <= (i*2)+1; x++) {
+			res += "#";
+		}
+		if (i < n-1) {res+="\n"}
+	}
 
 	return res;
 }
