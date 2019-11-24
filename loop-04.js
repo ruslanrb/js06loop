8b3e404ead1dc1cc22b18aced3dd0531ca1b1ee2
Артур <artbudikin@gmail.com>
2019-11-24 22:15:53 +0300

    4 работа

--- loop-04.js
+++ loop-04.js
@@ -1,17 +1,22 @@
 function pyramid(n) {
-	var res = ""
-	for(var i=0;i<n;i++)
+	var res = "";
+
+for 
+	(var i = 0; i < n; i++) 
+{
+	for 
+		(var k = 1; k <= n-i-1; k++) 
 	{
-		for(var k=1;k<=n-i-1;k++)
-		{
-			res+=" "
+	res += " ";
 		}
-		for(var x=1;x<=(i*2)+1;x++)
-		{
-			res+="#"
+for (var x = 1; x <= (i*2)+1; x++) 
+{
+	res += "#";
 		}
-		if(i<n-1){res+="\n"}
+	if (i < n-1) {res+="\n"}
 	}
-	return res
-}
+
+return res;
+	}
+
 module.exports = pyramid;
