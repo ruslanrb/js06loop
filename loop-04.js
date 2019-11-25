5bef927c8c164f22bb71df40b404b6a2b5e321c8
ganyavasiliev <54825242+ganyavasiliev@users.noreply.github.com>
2019-11-25 18:04:06 +0900

    сделал

--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,21 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
+    var n = n;
+    var res = "#";
+    var output="";
 
-	return res;
+    if(n>=1 && n<=20){
+    	for (var i = 0; i <n; i++){
+    		var myspace = "";
+    		for(let s = 0; s <(n-i-1); s++){
+    			myspace += " ";
+    		}
+    		for (var j = 1; j <= 2*i + 1; j++){
+    			output+=res;
+    		}
+    		console.log(myspace+output);
+        output="";;
+    	}
+    }
 }
 
 module.exports = pyramid;
