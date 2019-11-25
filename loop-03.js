0ace61ec87c5dde932c08c21b94cd52141cdeb4d
Mikhaylov3 <arthurmikhaylov023@gmail.com>
2019-11-25 20:27:33 +0900

    1,2,3

--- loop-03.js
+++ loop-03.js
@@ -1,8 +1,19 @@
 function gcd(m,n) {
-	var res;
-	// напишите код нахождения наибольшего общего делителя
-
-	return res;
+	m = abs(m);
+	n = abs(n);
+	if (m == n) {
+		return m;
+	}
+	while (m != n) {
+		l = m;
+		m = n;
+		if (l > n) {
+			n = l - n;
+		}
+		else {
+			n = n - l;
+		}
+	}
+	return m;
 }
-
 module.exports = gcd;
