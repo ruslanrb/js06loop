87a2421c4a83bc2f34398812e79a189834c9012a
Ustinov-Ki <54973177+Ustinov-Ki@users.noreply.github.com>
2019-11-25 21:51:10 +0900

    Add files via upload

---
+++ loop-03.js
@@ -0,0 +1,17 @@
+function gcd(m,n) {
+	var res;
+	// напишите код нахождения наибольшего общего делителя
+	while(m != 0 && n != 0){
+		if (m > n){
+			m %= n;
+		}
+		else{
+			n %= m;
+		}
+	}
+
+	res = m + n;
+	return res;
+}
+
+module.exports = gcd;
