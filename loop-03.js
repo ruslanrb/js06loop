3a28e3060739279f6e6eb8784f0381c77ad48b67
o-hash <noneideal@mail.ru>
2019-11-25 21:13:57 +0900

    Update loop-03.js

--- loop-03.js
+++ loop-03.js
@@ -1,7 +1,14 @@
 function gcd(m,n) {
 	var res;
 	// напишите код нахождения наибольшего общего делителя
-
+    while (m != 0 && n != 0) {
+          if (m > n){          
+          	m = m - n;        
+          }
+          else {
+          	n = n - m;
+          }
+    res = m + n;
 	return res;
 }
 
