23b8c501aef54df65edc751913a18b71ed58bf75
Darkhan <leontiev7000@mail.ru>
2019-11-25 19:22:27 +1000

    asd

--- loop-03.js
+++ loop-03.js
@@ -1,22 +1,18 @@
 function gcd(m,n) {
 	var res;
 	// напишите код нахождения наибольшего общего делителя
-	if ((m===0)||(n===0)){
-		return "АААаааа! Обнаружен НОООЛЬ!";
+while(m != 0 && n != 0)
+{
+	if (m > n){
+		m %= n;
 	}
-	else {
-		while(m!=n){
-		m=Math.abs(m)
-		n=Math.abs(n)
-		if (m>n) {
-			m=m-n
+	else{
+		n %= m;
 		}
-		else if (m<n){
-			n=n-m}
-		}
-	res=m
-	return res;
-}
+	}
+
+	res = m + n;
+return res;
 }
 
 module.exports = gcd;
--- loop-06.js
+++ loop-06.js
@@ -1,5 +1,44 @@
-function expDiff(nstr) {
+function expDiff(a) {
     // модуль разности между стандартной экспонентой и приближением в виде ряда
+    let exp = 0, i = 0;
+    do{
+    	if (i == 0){
+    		exp += 1;
+    	}
+    	else{
+    	if (abc((pow(a, i) / fact(i))) < 0.0001){
+    		break;
+    		}
+    	else{
+    		exp += pow(a, i) / fact(i);
+    		}
+        }
+    i++;
+   }while(true);
+   return Math.exp(a) - abc(exp);
+}
+
+function pow(n, a){
+	let num = 1;
+	for (let i = 0; i < a; i++){
+		num *= n;
+	}
+	return num;
+}
+
+function fact(n){
+	let a = 1;
+	for (let i = 2; i <= n; i++){
+		a *= i;
+	}
+	return a;
+}
+
+function abc(n){
+	if (n < 0){
+		n *= -1;
+	}
+	return n;
 }
 
 module.exports = expDiff;
