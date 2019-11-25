c24ab77a3bbbb15bace4e7b2b7e5a169472d290d
ruslanrb <54973228+ruslanrb@users.noreply.github.com>
2019-11-25 16:37:21 +0300

    loop-4.js

--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,16 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
+	var i = 0, j = 0;
+	var n = 32;
+	var space = "",
+	    reshotka = "";
 
-	return res;
+	while (i < n) {
+	    space = "";
+	    reshotka = "";
+	    for (j = 0; j < n - i; j++) space += " ";
+	    for (j = 0; j < 2 * i + 1; j++) reshotka += "#";
+	    console.log(space + reshotka);
+	    i++;
+	}
 }
-
 module.exports = pyramid;
