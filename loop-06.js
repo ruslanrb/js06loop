09c333a3b08a55d0585dd63d6272c79910307a41
ruslanrb <54973228+ruslanrb@users.noreply.github.com>
2019-11-25 16:39:42 +0300

    loop-06.js

--- loop-06.js
+++ loop-06.js
@@ -1,5 +1,23 @@
-function expDiff(nstr) {
-    // модуль разности между стандартной экспонентой и приближением в виде ряда
-}
+function expDiff(x) {
+    var slag = x;
+    var factorial = 1;
+    var term = 322228;
+    var summa = 1 + x;
+    var n = 2;
 
+    while (term > 0.0001) {
+        for(var i = 2; i <= n; i++) {
+		    slag *= x;
+    	    factorial *= i;
+    	}
+    	n++;
+        term = slag / factorial;
+        summa += term;
+    	slag = x;
+        factorial = 1;
+        if (term < 0)
+            term = term * -1;
+    }
+    return Math.abs(Math.exp(x) - summa)
+}
 module.exports = expDiff;
